90add4bff059ec81cd373d5dd4303ac63106b7dc
Mykeys27 <lmaki655@gmail.com>
2024-05-13 22:45:01 +0300

    form for login and register

--- users/forms.py
+++ users/forms.py
@@ -1,14 +1,23 @@
-from django.forms import ModelForm
-from .models import UserProfile
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
+class UserRegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
 
-
-class UserForm(ModelForm):
     class Meta:
-        model = UserProfile
-        fields = '__all__'
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
 
-# Check if the user is authenticated
-        def is_authenticated(self , user):
-            return user.is_authenticated
-        
+    def save(self, commit=True):
+        user = super(UserRegistrationForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
+
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(label="Username", max_length=30,
+                               widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'username'}))
+    password = forms.CharField(label="Password", max_length=30,
+                               widget=forms.PasswordInput(attrs={'class': 'form-control', 'name': 'password'}))
