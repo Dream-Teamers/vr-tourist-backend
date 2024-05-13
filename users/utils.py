0f0f68f03bec0a1ce5c7c42aea96a2e03bbad61b
Mykeys27 <lmaki655@gmail.com>
2024-05-13 23:41:08 +0300

    utils updated

--- users/utils.py
+++ users/utils.py
@@ -1,5 +1,5 @@
 from users.models import UserProfile
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def searchProfiles(request):
     search_query = ''
@@ -9,3 +9,13 @@ def searchProfiles(request):
 
     profiles = UserProfile.objects.filter(name__icontains=search_query)
     return profiles, search_query
+def paginateProfiles(request, profile):
+    paginator = Paginator(profiles, 3)
+    page = request.GET.get('page')
+    try:
+        profiles = paginator.page(page)
+    except PageNotAnInteger:
+        profiles = paginator.page(1)
+    except EmptyPage:
+        profiles = paginator.page(paginator.num_pages)
+    return profiles
