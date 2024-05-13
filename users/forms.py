fed53b871f2a52e7b1dddb7960ab1475d5c79a9b
Mykeys27 <lmaki655@gmail.com>
2024-05-13 20:25:57 +0300

    Test

--- users/forms.py
+++ users/forms.py
@@ -8,3 +8,8 @@ class UserForm(ModelForm):
     class Meta:
         model = UserProfile
         fields = '__all__'
+
+# Check if the user is authenticated
+        def is_authenticated(self , user):
+            return user.is_authenticated
+        
