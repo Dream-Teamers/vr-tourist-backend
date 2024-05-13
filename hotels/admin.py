3aefd2da798a0d53198f96d1c24d869f5a0f32ad
Mykeys27 <lmaki655@gmail.com>
2024-05-13 18:37:30 +0300

    models added to admin

--- hotels/admin.py
+++ hotels/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Hotel, Room, RoomBooking, RoomReview, RoomImage
 
-# Register your models here.
+admin.site.register(Hotel)
+admin.site.register(Room)
+admin.site.register(RoomBooking)
+admin.site.register(RoomReview)
+admin.site.register(RoomImage)
