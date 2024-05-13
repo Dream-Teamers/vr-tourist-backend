c4f63d4df3002019e7ec33aed02e6442da0a6541
amh-42 <anwarmisbaham@gmail.com>
2024-05-13 18:39:49 +0300

    added user models to admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import UserProfile, ActivityLog, PaymentMethod, Wishlist, Address
 # Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(ActivityLog)
+admin.site.register(PaymentMethod)
+admin.site.register(Wishlist)
+admin.site.register(Address)
