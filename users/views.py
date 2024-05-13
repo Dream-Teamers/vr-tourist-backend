4a53fc986e3c8808f63df6ed06e7fd8c61e9bfbb
amh-42 <anwarmisbaham@gmail.com>
2024-05-13 16:42:50 +0300

    updated users view

--- users/views.py
+++ users/views.py
@@ -1,6 +1,227 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+<<<<<<< HEAD
+from django.dispatch.dispatcher import receiver
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.urls import conf
+from django.db.models import Q
+from .models import Profile, Message
+from .forms import CustomUserCreationForm, ProfileForm, RoleForm, MessageForm
+from .utils import searchProfiles, paginateProfiles
 
-# Create your views here.
-def index(requests):
-    return HttpResponse("Hello World")
+
+def loginUser(request):
+    page = 'login'
+
+    if request.user.is_authenticated:
+        return redirect('profiles')
+
+    if request.method == 'POST':
+        username = request.POST['username'].lower()
+        password = request.POST['password']
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'Username does not exist')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect(request.GET['next'] if 'next' in request.GET else 'account')
+
+        else:
+            messages.error(request, 'Username OR password is incorrect')
+
+    return render(request, 'users/login_register.html')
+
+
+def logoutUser(request):
+    logout(request)
+    messages.info(request, 'User was logged out!')
+    return redirect('login')
+
+
+def registerUser(request):
+    page = 'register'
+    form = CustomUserCreationForm()
+
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+
+            messages.success(request, 'User account was created!')
+
+            login(request, user)
+            return redirect('edit-account')
+
+        else:
+            messages.success(
+                request, 'An error has occurred during registration')
+
+    context = {'page': page, 'form': form}
+    return render(request, 'users/login_register.html', context)
+
+
+def profiles(request):
+    profiles, search_query = searchProfiles(request)
+
+    custom_range, profiles = paginateProfiles(request, profiles, 3)
+    context = {'profiles': profiles, 'search_query': search_query,
+               'custom_range': custom_range}
+    return render(request, 'users/profiles.html', context)
+
+
+def userProfile(request, pk):
+    profile = Profile.objects.get(id=pk)
+
+    topRoles = profile.role_set.exclude(description__exact="")
+    otherRoles = profile.role_set.filter(description="")
+
+    context = {'profile': profile, 'topRoles': topRoles,
+               "otherRoles": otherRoles}
+    return render(request, 'users/user-profile.html', context)
+
+
+@login_required(login_url='login')
+def userAccount(request):
+    profile = request.user.profile
+
+    roles = profile.role_set.all()
+    projects = profile.project_set.all()
+
+    context = {'profile': profile, 'roles': roles, 'projects': projects}
+    return render(request, 'users/account.html', context)
+
+
+@login_required(login_url='login')
+def editAccount(request):
+    profile = request.user.profile
+    form = ProfileForm(instance=profile)
+
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            form.save()
+
+            return redirect('account')
+
+    context = {'form': form}
+    return render(request, 'users/profile_form.html', context)
+
+
+@login_required(login_url='login')
+def createRole(request):
+    profile = request.user.profile
+    form = RoleForm()
+
+    if request.method == 'POST':
+        form = RoleForm(request.POST)
+        if form.is_valid():
+            role = form.save(commit=False)
+            role.owner = profile
+            role.save()
+            messages.success(request, 'Role was added successfully!')
+            return redirect('account')
+
+    context = {'form': form}
+    return render(request, 'users/role_form.html', context)
+
+
+@login_required(login_url='login')
+def updateRole(request, pk):
+    profile = request.user.profile
+    role = profile.role_set.get(id=pk)
+    form = RoleForm(instance=role)
+
+    if request.method == 'POST':
+        form = RoleForm(request.POST, instance=role)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Role was updated successfully!')
+            return redirect('account')
+
+    context = {'form': form}
+    return render(request, 'users/role_form.html', context)
+
+
+@login_required(login_url='login')
+def deleteRole(request, pk):
+    profile = request.user.profile
+    role = profile.role_set.get(id=pk)
+    if request.method == 'POST':
+        role.delete()
+        messages.success(request, 'Role was deleted successfully!')
+        return redirect('account')
+
+    context = {'object': role}
+    return render(request, 'delete_template.html', context)
+
+
+@login_required(login_url='login')
+def inbox(request):
+    profile = request.user.profile
+    messageRequests = profile.messages.all()
+    unreadCount = messageRequests.filter(is_read=False).count()
+    context = {'messageRequests': messageRequests, 'unreadCount': unreadCount}
+    return render(request, 'users/inbox.html', context)
+
+
+@login_required(login_url='login')
+def viewMessage(request, pk):
+    profile = request.user.profile
+    message = profile.messages.get(id=pk)
+    if message.is_read == False:
+        message.is_read = True
+        message.save()
+    context = {'message': message}
+    return render(request, 'users/message.html', context)
+
+
+def createMessage(request, pk):
+    recipient = Profile.objects.get(id=pk)
+    form = MessageForm()
+
+    try:
+        sender = request.user.profile
+    except:
+        sender = None
+
+    if request.method == 'POST':
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.sender = sender
+            message.recipient = recipient
+
+            if sender:
+                message.name = sender.name
+                message.email = sender.email
+            message.save()
+
+            messages.success(request, 'Your message was successfully sent!')
+            return redirect('user-profile', pk=recipient.id)
+
+    context = {'recipient': recipient, 'form': form}
+    return render(request, 'users/message_form.html', context)
+=======
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # You can add any post-sign-up logic here
+            return redirect('login')  # Redirect to login page after successful sign-up
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
+>>>>>>> 22772f69ccc23f13ebac30d0b7f0e34459f65967
