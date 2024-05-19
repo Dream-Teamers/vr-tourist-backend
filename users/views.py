1d14433550f39f7ed1d2597da03193bcb3cb38d0
Mykeys27 <lmaki655@gmail.com>
2024-05-19 16:51:16 +0300

    forms modified

--- users/views.py
+++ users/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
-from .forms import UserLoginForm
-from .models import UserProfile
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import UserProfile
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.contrib.auth.models import User
+
+
+
+
+
+
+
 
 def registerPage(request):
     form = UserCreationForm()
