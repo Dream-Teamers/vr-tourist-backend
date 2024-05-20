9c8205d1ea6b9e191fa67273b387d904ae3e0735
amh-42 <anwarmisbaham@gmail.com>
2024-05-21 02:45:36 +0300

    added/ updated the update vr url

--- vr_experience/urls.py
+++ vr_experience/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.vr_experiences, name='vrs'),
     path('vr/<str:pk>/', views.vr_experience, name='single-vr'),
     path('create-vr/', views.createVR, name='create-vr'),
-    path('update-vr/<str:pk>/', views.updateVR, name='update-vr'),
+    path('update-vr/<str:title>/', views.updateVR, name='update-vr'),
 ]
