948587b2180b68bf70b48ffde5b59c4ba5ebe2a7
amh-42 <anwarmisbaham@gmail.com>
2024-05-21 02:45:11 +0300

    changed the vr review to vr rating

--- vr_experience/models.py
+++ vr_experience/models.py
@@ -7,8 +7,7 @@ class VRExperience(models.Model):
     duration = models.PositiveIntegerField(null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2,null=True, blank=True)
     tags = models.ManyToManyField('Tag', blank=True)
-    vote_total = models.IntegerField(default=0, null=True, blank=True)
-    vote_ratio = models.IntegerField(default=0, null=True, blank=True)
+    rating = models.IntegerField(default=5)
     locations = models.TextField(null=True, blank=True)
     image_url = models.CharField(max_length=2000, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
@@ -17,20 +16,22 @@ class VRExperience(models.Model):
     def __str__(self) -> str:
         return self.title
     
-class VRReview(models.Model):
+class VRRating(models.Model):
     VOTE_TYPE = (
-        ('up', 'Up Vote'),
-        ('down', 'Down Vote')
+        ('5', '5'),
+        ('4', '4'),
+        ('3', '3'),
+        ('2', '2'),
+        ('1', '1')
     )
-    rating = models.CharField(max_length=200, choices=VOTE_TYPE)
-    # user = models.ForeignKey(User, on_delete=models.CASCADE)
-    vr_experience = models.ForeignKey(VRExperience, on_delete=models.CASCADE)
+    value = models.CharField(max_length=200, choices=VOTE_TYPE, default='5')
+    vr_experience = models.ForeignKey(VRExperience  , on_delete=models.CASCADE , default=None)
     comment = models.TextField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     _id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
     def __str__(self) -> str:
-        return self.rating
+        return self.value
     
 class Tag(models.Model):
     name = models.CharField(max_length=200)
