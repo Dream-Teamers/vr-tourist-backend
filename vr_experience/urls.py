3fdaa409d730bceb92298cea990670bb6cfdbb8c
amh-42 <anwarmisbaham@gmail.com>
2024-05-20 21:59:20 +0300

    fixed the urls for main landing page

--- vr_experience/urls.py
+++ vr_experience/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import  views
 
 urlpatterns = [
-    path('vrs/', views.vr_experiences, name='vrs'),
+    path('', views.vr_experiences, name='vrs'),
     path('vr/<str:pk>/', views.vr_experience, name='single-vr'),
     path('create-vr/', views.createVR, name='create-vr'),
     path('update-vr/<str:pk>/', views.updateVR, name='update-vr'),
