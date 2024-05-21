16e2a983539c92582df54d18b5087a829d3671dc
amh-42 <anwarmisbaham@gmail.com>
2024-05-21 15:49:54 +0300

    updated the models for user Account

--- users/models.py
+++ users/models.py
@@ -1,14 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
+import uuid
 
 # Create your models here.
 class UserAccount(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
-    name = models.CharField(max_length=200, blank=True, null=True)
-    profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)
+    ROLE_CHOICES = (
+        ('tourist', 'Tourist'),
+        ('tour_agency', 'Tour Agent'),
+        ('hotle', 'Hotel Manger'),
+    )
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=200, choices=ROLE_CHOICES, default='tourist')
     bio = models.TextField(null=True, blank=True)
     date_of_birth = models.DateField(null=True, blank=True)
     contact_info = models.CharField(max_length=100, null=True, blank=True)
+    _id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    def __str__(self) -> str:
+        return self.role
 
     
 
