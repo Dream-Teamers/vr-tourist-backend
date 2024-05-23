61995a3fcc87eabf3f1b018eb087a2a956bbb2dc
amh-42 <anwarmisbaham@gmail.com>
2024-05-23 23:33:37 +0300

    implemented api view

--- apis/views.py
+++ apis/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from vr_experience.models import VRExperience
+from hotels.models import Hotel
+from agencies.models import TourAgency
+from users.models import UserAccount
+from .serializers import VRSerializer, HotelSerializer
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        'GET /api/vrs',
+        'GET /api/vr/:id',
+        'GET /api/hotels',
+        'GET /api/hotel/:id',
+        'GET /api/tours',
+        'GET /api/tour/:id',
+        'GET /api/agencies',
+        'GET /api/agency/:id',
+        'GET /api/rooms',
+        'GET /api/room/:id',
+        'GET /api/users',
+        'GET /api/user/:id'
+    ]
+    return Response(routes)
 
-# Create your views here.
+@api_view(['GET'])
+def getVRs(request):
+    vrs = VRExperience.objects.all()
+    serialized = VRSerializer(vrs, many=True)
+    return Response(serialized.data)
+
+@api_view(['GET'])
+def getVR(request, pk):
+    vr = VRExperience.objects.get(title=pk)
+    print(vr)
+    serialized = VRSerializer(vr)
+    return Response(serialized.data)
+
+@api_view(['GET'])
+def getHotels(request):
+    hotels = Hotel.objects.all()
+    serialized = HotelSerializer(hotels, many=True)
+    return Response(serialized.data)
