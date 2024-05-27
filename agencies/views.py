940012027566503ba2b0aceb983246b6d7b0c43c
Mykeys27 <lmaki655@gmail.com>
2024-05-27 14:44:33 +0300

    agencies template updated

--- agencies/views.py
+++ agencies/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 # from .forms import VRForm, RatingForm
 from .models import Tour, TourAgency, TourBooking, TourReview
 from django.contrib.auth.decorators import login_required
-
+from users.forms import UserUpdateForm, HelpSupportForm
 
 def agenciesPage(request):
     agencies = TourAgency.objects.all()
@@ -29,3 +29,39 @@ def agencyPage(request,pk):
 @login_required(login_url='login')
 def tour_agency_dashboard(request):
     return render(request, 'agencies/tour_agency_dashboard.html')
+
+@login_required(login_url='login')
+def add_tour_package(request):
+    # Implement functionality to add hotel listing
+    return render(request, 'agencies/add_tour_package.html')
+
+@login_required(login_url='login')
+def update_tour_package(request):
+    # Implement functionality to update hotel listing
+    return render(request, 'agencies/update_tour_package.html')
+
+@login_required(login_url='login')
+def view_tour_package(request):
+    # Implement functionality to delete hotel listing
+    return render(request, 'agencies/view_tour_package.html')
+
+@login_required(login_url='login')
+def manage_tour_reviews(request):
+    # Implement functionality to view hotel bookings
+    return render(request, 'agencies/manage_tour_reviews.html')
+
+
+@login_required(login_url='login')
+def notifications(request):
+    return render(request, 'agencies/notifications.html')
+
+@login_required(login_url='login')
+def help_support(request):
+    if request.method == 'POST':
+        form = HelpSupportForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('help-support')
+    else:
+        form = HelpSupportForm()
+    return render(request, 'agencies/help_support.html', {'form': form})
