ba6c25531526ae899f229603266f7949ea835bc3
amh-42 <anwarmisbaham@gmail.com>
2024-05-29 17:50:11 +0300

    edited the rating url

--- vr_experience/urls.py
+++ vr_experience/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('vr/<str:pk>/', views.vr_experience, name='single-vr'),
     path('create-vr/', views.createVR, name='create-vr'),
     path('update-vr/<str:title>/', views.updateVR, name='update-vr'),
-    path('rate-vr/<str:pk>/', views.rateVR, name='rate-vr'),
+    path('rate-vr/<str:title>/', views.rateVR, name='rate-vr'),
     path('delete/<str:title>/', views.deleteVR, name='delete-vr'),
     path('book-vr/<str:pk>/', views.book_vr_experience, name='book_vr_experience'),
 ]
