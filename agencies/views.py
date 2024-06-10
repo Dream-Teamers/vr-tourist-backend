745b07a2067ef357516b9b154b9741d70989d09d
AMH-42 <anwarmisbaham@gmail.com>
2024-06-10 08:27:13 +0300

    minor changes

--- agencies/views.py
+++ agencies/views.py
@@ -3,10 +3,17 @@ from rest_framework import generics
 from .models import AgencyRating, Tour, TourAgency, TourBooking
 from apis.serializers import TourAgencySerializer, TourSerializer
 
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+from apis.permissions import IsAuthenticatedOrReadOnly, IsAdminUser
+
 # Create your views here.
 class TourAgencyListCreate(generics.ListCreateAPIView):
     queryset = TourAgency.objects.all()
     serializer_class = TourAgencySerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAdminUser]
     
     
 class TourAgencyRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
@@ -14,22 +21,36 @@ class TourAgencyRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = TourAgencySerializer
     lookup_field = 'pk'
     
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAdminUser]
+    
 class TourListCreate(generics.ListCreateAPIView):
     queryset = Tour.objects.all()
     serializer_class = TourSerializer
+    
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAdminUser]
     
 class TourRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = Tour.objects.all()
     serializer_class = TourSerializer
     lookup_field = 'pk'
     
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAdminUser]
+    
 class TourBookingListCreate(generics.ListCreateAPIView):
     queryset = Tour.objects.all()
     serializer_class = TourSerializer
+    
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAdminUser]
     
 class TourBookingRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = Tour.objects.all()
     serializer_class = TourSerializer
     lookup_field = 'pk'
     
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAdminUser]
 
--- vrs/views.py
+++ vrs/views.py
@@ -9,21 +9,28 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, generics
 from rest_framework.authtoken.models import Token
+from apis.permissions import IsAuthenticatedOrReadOnly, IsAdminUser
 
 # Create your views here.
 class VRListCreate(generics.ListCreateAPIView):
     queryset = VR.objects.all()
     serializer_class = VRSerializer
     
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAdminUser]
     
 class VRRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = VR.objects.all()
     serializer_class = VRSerializer
     lookup_field = 'pk'
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAdminUser]
     
 
 
 class VRList(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAdminUser]
     def get(self, request, format=None):
         # Get the title form the query
         title = request.query_params.get('title', '')
@@ -44,20 +51,29 @@ class VRBookingListCreate(generics.ListCreateAPIView):
     queryset = VRBooking.objects.all()
     serializer_class = VRBookingSerializer
     
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAdminUser]
     
 class VRBookingRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = VRBooking.objects.all()
     serializer_class = VRBookingSerializer
     lookup_field = 'pk'
     
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAdminUser]
     
     
 class VRRatingListCreate(generics.ListCreateAPIView):
     queryset = VRRating.objects.all()
     serializer_class = VRRatingSerializer
     
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAdminUser]
     
 class VRRatingRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = VRRating.objects.all()
     serializer_class = VRRatingSerializer
     lookup_field = 'pk'
+    
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAdminUser]
